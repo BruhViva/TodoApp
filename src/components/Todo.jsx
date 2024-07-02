34c8e69ee10f1fd029cc4ecd2d3f5c8e262240b0
BruhViva <viwaj562@gmail.com>
2024-07-02 13:19:54 +0530

    font upgradation

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -71,7 +71,7 @@ useEffect(()=>{
 
 {/*------input box-----*/}
     <div className='flex items-center my-7 bg-red-100 rounded-full'>
-        <input ref = {inputRef} className='bg-transparent border-0 outline-none flex-1 h-14
+        <input ref = {inputRef} className='bg-transparent border-0 outline-none flex-1 h-14 font-extrabold
         pl-6 pr-2 placeholder:text-slate-600'type="text" placeholder='Add your task' />
         <button onClick={add} className='border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer'>ADD +</button>
     </div>
--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -9,7 +9,7 @@ const TodoItems = ({text, id, isComplete, deleteTodo, toggle}) => {
     <div className='flex items-center my-3 gap-2'>
         <div onClick={() =>{toggle(id)}}className='flex flex-1 items-center cursor-pointer'>
             <img src={isComplete ? tick : not_tick} alt="" className='w-7'/>
-            <p className={`tex-slate-700 ml-4 text-[17px] decoration-slate-500
+            <p className={`tex-slate-700 ml-4 font-semibold text-[17px] decoration-slate-500
               ${isComplete ? "line-through" : ""}`}>
                 {text}</p>
         </div>
